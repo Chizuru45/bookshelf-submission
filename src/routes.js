c69735c4cbfde2b320c7a956f0ab750b115edd69
Benyamn Netanyahu Immanuel Partogi Siregar <81239166+Chizuru45@users.noreply.github.com>
2022-03-07 00:47:20 +0700

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,30 @@
+const { addBookHandler, getAllBooksHandler, getDetailBookHandler, editIdBookHandler, deleteBookByIdHandler } = require('./handler');
+const routes = [
+    {
+      method: 'POST',
+      path: '/books',
+      handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooksHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{bookid}',
+        handler: getDetailBookHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{bookId}',
+        handler: editIdBookHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{bookId}',
+        handler: deleteBookByIdHandler,
+    },
+  
+];
+  module.exports = routes;
